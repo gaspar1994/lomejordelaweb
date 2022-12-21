e839bebe1bf4431f323ec10ba52484d941144150
unknown <theking920602@gmail.com>
2022-12-21 14:02:13 -0600

    Revert "Update SiteLogo.vue"
    This reverts commit a1fd7e85ceec57f106f801e6677b52df540facb4.

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -11,5 +11,22 @@
     </div>
   </div>
 </template>
-
+<script>
+export default {
+  name: 'SiteLogo',
+  data() {
+    return {
+      frames: 1,
+      currentFrame: 1,
+      blinking: true
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      const rand = Math.ceil(Math.random() * 10)
+      this.blinking = rand % 2
+    }, 1000)
+  }
+}
+</script>
 
