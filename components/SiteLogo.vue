85a252b24923703cd4bd5e51dba464c6f6866694
unknown <theking920602@gmail.com>
2022-12-21 13:55:59 -0600

    Ultimo cambio

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -33,29 +33,4 @@ export default {
   }
 }
 </script>
-<style lang="scss">
-@keyframes logoBlink {
-  100% {
-    transform: translateY(-360px);
-  }
-}
-.logo-wrapper {
-  position: relative;
-  height: 50px;
-  overflow: hidden;
-  width: 200px;
-  .logo-inner {
-    position: absolute;
-    height: 500px;
-    img {
-      display: block;
-      height: 40px;
-      margin: 5px;
-      max-height: initial;
-    }
-    &.blink {
-      animation: logoBlink 0.2s steps(8, end) alternate;
-    }
-  }
-}
-</style>
+
