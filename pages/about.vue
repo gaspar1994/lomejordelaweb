08697c1468e25f6c41c9b485a089d08a8953ca5a
unknown <theking920602@gmail.com>
2022-12-21 12:35:04 -0600

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -8,81 +8,17 @@
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Aqui va el titulo</h3>
+          <h3>Lo mejor de la Web</h3>
           <p>
             <strong>
-              Edit this page in<code>/pages/about.vue</code>to fit your needs.
+              ¡Bienvenidos a nuestra página de videos deportivos! Aquí encontrarás una amplia variedad de contenido deportivo, desde partidos en vivo y resúmenes de juegos hasta entrevistas con jugadores y entrenadores. También tenemos tutoriales y consejos para mejorar tu rendimiento en distintos deportes.
+
+Nuestro objetivo es brindar a los aficionados al deporte la mejor experiencia posible, y para ello contamos con un equipo de profesionales que se encarga de seleccionar y editar el contenido más interesante y de calidad. Además, nuestra plataforma está diseñada para ser fácil de usar y accesible desde cualquier dispositivo.
+
+Si eres un apasionado del deporte, no puedes dejar de visitar nuestra página. ¡Te aseguramos que encontrarás mucho material interesante y divertido!
             </strong>
           </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
-          </p>
-
-          <h3>Another Fake Heading</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
-          </p>
-          <quote-card />
-          <h3>Still More Fake Content</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit! Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit.
-            <strong>Alias aut facere harum ipsam</strong> quibusdam quidem
-            quisquam saepe? Alias architecto blanditiis consequatur consequuntur
-            dicta ipsa iste modi provident, quod, similique veritatis.
-          </p>
-        </div>
-
-        <div class="tile is-ancestor">
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                Cool Stuff We Do
-              </p>
-              <p class="subtitle">
-                Sooo Cool
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                Other Cool Things
-              </p>
-              <p class="subtitle">
-                Just as cool
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                All the cool
-              </p>
-              <p class="subtitle">
-                that is all
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-        </div>
+          
       </template>
 
       <template v-slot:sidebar>
