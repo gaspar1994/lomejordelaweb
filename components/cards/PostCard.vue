2cb17565f514ca886a8f626ccfbe00b10a9e3d7b
unknown <theking920602@gmail.com>
2022-12-21 12:03:38 -0600

    autor

--- components/cards/PostCard.vue
+++ components/cards/PostCard.vue
@@ -13,7 +13,7 @@
       v-if="author && $siteConfig.posts.displayAuthor"
       class="author-wrapper"
     >
-      <strong>Author:</strong> {{ author }} |
+      <strong>Autor:</strong> {{ author }} |
     </span>
     <span v-if="date" class="date-wrapper">
       <strong>Published on:</strong> {{ datePretty }}
