572732e98e298bdfaee9d8b216eb8abd7013194c
unknown <theking920602@gmail.com>
2022-12-21 12:41:19 -0600

    imagen about

--- pages/about.vue
+++ pages/about.vue
@@ -16,7 +16,8 @@
 Nuestro objetivo es brindar a los aficionados al deporte la mejor experiencia posible, y para ello contamos con un equipo de profesionales que se encarga de seleccionar y editar el contenido más interesante y de calidad. Además, nuestra plataforma está diseñada para ser fácil de usar y accesible desde cualquier dispositivo.
 <br><br>
 Si eres un apasionado del deporte, no puedes dejar de visitar nuestra página. ¡Te aseguramos que encontrarás mucho material interesante y divertido!
-          
+<br><br>
+<img src="https://image.jimcdn.com/app/cms/image/transf/dimension=715x10000:format=png/path/s69e2c96da401cd47/image/i636ef2db36ed52f2/version/1572744882/image.png" border="1" alt="Este es el ejemplo de un texto alternativo" width="400" height="300">          
           </p>
           
       </template>
